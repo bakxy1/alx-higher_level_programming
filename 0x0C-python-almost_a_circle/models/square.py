9218ca5c52310c901579fa54f69fe1227098e7bf
Leykun Gizaw <leykungizaw@gmail.com>
2024-08-06 17:47:33 +0300

    feat(Task 12): Override update method for square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -20,6 +20,29 @@ class Square(Rectangle):
         """Setter for size"""
         self.width, self.height = size, size
 
+    def update(self, *args, **kwargs):
+        """Update attributes of square"""
+        if len(args) > 0:
+            for idx in range(len(args)):
+                if idx == 0:
+                    self.id = args[idx]
+                elif idx == 1:
+                    self.size = args[idx]
+                elif idx == 2:
+                    self.x = args[idx]
+                elif idx == 3:
+                    self.y = args[idx]
+        else:
+            for k, v in kwargs.items():
+                if k == "id":
+                    self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
+
     def __str__(self):
         """Overrides Rectangle's string method"""
         return f"[Square] ({self.id}) {self.x}/{self.y} " + f"- {self.width}"
