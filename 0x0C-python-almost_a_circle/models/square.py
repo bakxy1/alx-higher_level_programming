e930e9e3c4ad2e9917e7fb62bbfdf739eb89dc1b
Leykun Gizaw <leykungizaw@gmail.com>
2024-08-06 17:42:13 +0300

    feat(Task 11): Add getter and setter for size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,6 +10,16 @@ class Square(Rectangle):
         """Constructor"""
         super().__init__(width=size, height=size, x=x, y=y, id=id)
 
+    @property
+    def size(self):
+        """Getter for size"""
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        """Setter for size"""
+        self.width, self.height = size, size
+
     def __str__(self):
         """Overrides Rectangle's string method"""
         return f"[Square] ({self.id}) {self.x}/{self.y} " + f"- {self.width}"
