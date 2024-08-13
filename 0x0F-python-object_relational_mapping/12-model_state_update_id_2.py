8936a401fc26b8cd9b1e5b10dc67c5d73280647d
Leykun Gizaw <leykungizaw@gmail.com>
2024-08-13 16:18:47 +0300

    feat(Task 12): Update record with ID 2

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module lists first state in `hbtn_0e_6_usa`.`states` table."""
+
+from sqlalchemy.util.langhelpers import warn
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine(
+        f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+    )
+
+    session = sessionmaker(bind=engine)()
+
+    session.query(State).filter_by(id=2).update({"name": "New Mexico"})
+
+    session.commit()
+    session.close()
