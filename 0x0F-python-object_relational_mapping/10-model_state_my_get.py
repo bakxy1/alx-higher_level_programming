2b6f9cc896c5a25c37bfec05ba6cfa10fd142d98
Leykun Gizaw <leykungizaw@gmail.com>
2024-08-13 15:58:03 +0300

    feat(Task 10): Get id of a state

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""List all state records"""
+
+if __name__ == "__main__":
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    from model_state import Base, State
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+    Base.metadata.create_all(engine)
+
+    session = sessionmaker(bind=engine)()
+
+    states = []
+    for state in session.query(State).filter_by(name=sys.argv[4]):
+        states.append(state)
+
+    if states:
+        print(states[0].id)
+    else:
+        print("Not found")
